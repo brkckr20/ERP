018b627d1e1aec6092623ca6ff0f8c2420d22768
brkckr20 <burak2043@gmail.com>
2023-04-07 12:10:14 +0300

    .

--- frontend/src/pages/SatinAlma/AksesuarSatinAlma.jsx
+++ frontend/src/pages/SatinAlma/AksesuarSatinAlma.jsx
@@ -27,10 +27,15 @@ const AksesuarSatinAlma = () => {
                     </div>
                     <div className="w-1/5">
                         <LabelInput label="Sevk Eden :" />
-                        <LabelInput label="Termin :" />
+                        <LabelInput label="Teslim Alan :" />
+                        <LabelInput label="Talimat Onay :" type="checkbox" />
                         <LabelInput label="Onaylayan :" />
                         <LabelInput label="Onay Tarihi :" />
+                    </div>
+                    <div className="w-1/5">
+                        <LabelInput label="Talimat No :" />
                         <LabelInput label="İşlem Cinsi :" />
+                        <LabelInput label="Kayıt No :" />
                     </div>
                     <div className="w-1/5 flex gap-x-2">
                         <label className='inline-block max-w-[200px] w-full'>Açıklama : </label>
@@ -38,8 +43,57 @@ const AksesuarSatinAlma = () => {
                     </div>
                 </div>
             </div>
-            <div className='flex-1 bg-pink-400'>kalem kısmı</div>
-            <div className='flex-1 bg-green-400'>havuz kısmı</div>
+            <div className='flex-1 bg-pink-100'>
+                <table className='overflow-x-scroll'>
+                    <thead>
+                        <tr >
+                            <td>Sil</td>
+                            <td className='w-[320px]'>Aks. Kodu</td>
+                            <td className='w-[320px]'>Aks. Cinsi</td>
+                            <td className='w-[320px]'>Aks. Ebat</td>
+                            <td className='w-[320px]'>Aks. Grup</td>
+                            <td className='w-[320px]'>Aks. Renk</td>
+                            <td className='w-[320px]'>Ebat / Beden</td>
+                            <td className='w-[320px]'>Özellik 1</td>
+                            <td className='w-[320px]'>Özellik 2</td>
+                            <td className='w-[320px]'>Özellik 3</td>
+                            <td className='w-[320px]'>Özellik 4</td>
+                            <td className='w-[320px]'>Sip. No</td>
+                            <td className='w-[320px]'>G. Miktar</td>
+                            <td className='w-[320px]'>Birim</td>
+                            <td className='w-[320px]'>G. Fiyat</td>
+                            <td className='w-[320px]'>G. Döviz</td>
+                            <td className='w-[320px]'>Termin Tarihi</td>
+                            <td className='w-[320px]'>Kalem Açıklama</td>
+                            <td className='w-[320px]'>Kalem Kayıt No</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td></td>
+                            <td className='w-[120px]'>SATINALMA</td>
+                            <td className='w-[120px]'>stc111</td>
+                            <td className='w-[120px]'>GENEL</td>
+                            <td className='w-[120px]'>STICKER</td>
+                            <td className='w-[120px]'></td>
+                            <td className='w-[120px]'>20-12</td>
+                            <td className='w-[120px]'>3'LÜ BİTİŞİK</td>
+                            <td className='w-[120px]'>KUŞE</td>
+                            <td className='w-[120px]'>ÖZ2</td>
+                            <td className='w-[120px]'>ÖZ3</td>
+                            <td className='w-[120px]'>SİPNO</td>
+                            <td className='w-[120px]'>80000</td>
+                            <td className='w-[120px]'>AD</td>
+                            <td className='w-[120px]'>0,36</td>
+                            <td className='w-[120px]'>TL</td>
+                            <td className='w-[120px]'>TER.Tarih</td>
+                            <td className='w-[120px]'>KALEM AÇIKLAMA</td>
+                            <td className='w-[120px]'>KALEM KAYIT NO</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div className='flex-1 bg-green-200'>havuz kısmı</div>
         </div>
     )
 }
